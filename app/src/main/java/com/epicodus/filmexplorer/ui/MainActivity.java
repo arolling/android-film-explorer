5a48895f74cf0481721f9925c4615e6dfc013dab
Anna Becker and Abby Rolling <anna.r.becker@gmail.com>
2016-04-27 11:37:41 -0700

    Setup main activity and result activity

--- app/src/main/java/com/epicodus/filmexplorer/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/filmexplorer/ui/MainActivity.java
@@ -1,15 +1,39 @@
 package com.epicodus.filmexplorer.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import com.epicodus.filmexplorer.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.searchButton) ImageButton mSearchButton;
+    @Bind(R.id.searchQuery)EditText mSearchEditText;
+    @Bind(R.id.spinner) Spinner mSearchSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ResultListActivity.class);
+                String search = mSearchEditText.getText().toString();
+                String type = mSearchSpinner.getSelectedItem().toString();
+                intent.putExtra("search", search);
+                intent.putExtra("type", type);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/epicodus/filmexplorer/ui/ResultListActivity.java
+++ app/src/main/java/com/epicodus/filmexplorer/ui/ResultListActivity.java
@@ -1,15 +1,55 @@
 package com.epicodus.filmexplorer.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.epicodus.filmexplorer.R;
+import com.epicodus.filmexplorer.services.MovieService;
+
+import java.io.IOException;
+
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class ResultListActivity extends AppCompatActivity {
+    public static final String TAG = ResultListActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_list);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String search = intent.getStringExtra("search");
+        String type = intent.getStringExtra("type");
+        searchMovies(search, type);
+    }
+
+    private void searchMovies(String search, String type){
+        MovieService movieService = new MovieService();
+
+        movieService.searchMovies(search, type, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
